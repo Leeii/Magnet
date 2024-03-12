4d86f900daabd5c34659450b7090245f4fd85ae3
leeyi <136306413@qq.com>
2024-03-12 15:37:28 +0800

    optimize code

--- magnet/src/main/java/com/magnetwidgets/magnet/impl/BackgroundPiece.kt
+++ magnet/src/main/java/com/magnetwidgets/magnet/impl/BackgroundPiece.kt
@@ -3,48 +3,28 @@ package com.magnetwidgets.magnet.impl
 import android.view.View
 import android.widget.RemoteViews
 import com.magnetwidgets.magnet.Piece
-import com.magnetwidgets.magnet.illegalArgument
+import com.magnetwidgets.magnet.extension.getInt
 import com.widgetsmagnet.ksp.MagnetPiece
 
 @MagnetPiece("setBackgroundResource", MagnetPiece.ValueType.Int)
 class BackgroundResourcePiece : Piece {
     override fun updateView(view: View, vararg values: Any) {
-        val backgroundResource = values.first()
-        if (backgroundResource is Int) {
-            view.setBackgroundResource(backgroundResource)
-        } else {
-            illegalArgument("dest is not background resource id.")
-        }
+        view.setBackgroundResource(values.getInt())
     }
 
     override fun updateRemoteView(views: RemoteViews, viewId: Int, vararg values: Any) {
-        val backgroundResource = values.first()
-        if (backgroundResource is Int) {
-            views.setInt(viewId, "setBackgroundResource", backgroundResource)
-        } else {
-            illegalArgument("dest is not background resource id.")
-        }
+        views.setInt(viewId, "setBackgroundResource", values.getInt())
     }
 }
 
 @MagnetPiece("setBackgroundColor", MagnetPiece.ValueType.Int)
 class BackgroundColorPiece : Piece {
     override fun updateView(view: View, vararg values: Any) {
-        val backgroundColor = values.first()
-        if (backgroundColor is Int) {
-            view.setBackgroundColor(backgroundColor)
-        } else {
-            illegalArgument("dest is not background resource id.")
-        }
+        view.setBackgroundColor(values.getInt())
     }
 
     override fun updateRemoteView(views: RemoteViews, viewId: Int, vararg values: Any) {
-        val backgroundColor = values.first()
-        if (backgroundColor is Int) {
-            views.setInt(viewId, "setBackgroundColor", backgroundColor)
-        } else {
-            illegalArgument("dest is not background resource id.")
-        }
+        views.setInt(viewId, "setBackgroundColor", values.getInt())
     }
 }
 
